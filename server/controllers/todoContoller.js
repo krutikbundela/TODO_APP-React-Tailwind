710cc2f2f4998b1430cb297c7a497fca33231be7
Krutik Bundela <krutikbundela2001@gmail.com>
2024-02-07 11:51:46 +0530

    Created API  for Task Complete

--- server/controllers/todoContoller.js
+++ server/controllers/todoContoller.js
@@ -1,62 +1,80 @@
 import { errorHandler } from "../utils/error.js";
-import Todo from "../models/todoModel.js"
+import Todo from "../models/todoModel.js";
 
-export const createTask = async (req, res , next) => {
-    
-    try {
-      const {task} = req.body;
-      console.log("createTask ~ task:", task);
-    if(!task){
-       return next(errorHandler(400,"Please Enter Task"));
+export const createTask = async (req, res, next) => {
+  try {
+    const { task } = req.body;
+    console.log("createTask ~ task:", task);
+    if (!task) {
+      return next(errorHandler(400, "Please Enter Task"));
     }
 
     const newTodo = new Todo({
-        task,
-    })
+      task,
+    });
 
     await newTodo.save();
 
-     res.status(200).json({
-       success: true,
-       message: "Task Created",
-     });
-
+    res.status(200).json({
+      success: true,
+      message: "Task Created",
+    });
   } catch (error) {
     next(error);
   }
 };
 
-export const getAllTask = async(req,res,next) =>{
-    try {
-        const allTask = await Todo.find();
+export const getAllTask = async (req, res, next) => {
+  try {
+    const allTask = await Todo.find();
 
-        res.status(200).json(allTask);
-    } catch (error) {
-        next(error);
-    }
+    res.status(200).json(allTask);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const deleteTask = async(req,res,next) =>{
-    try {
-        const {id} = req.body;
+export const deleteTask = async (req, res, next) => {
+  try {
+    const { id } = req.body;
 
-        if(!id){
-            return next(errorHandler(400,"Id Not Found"))
-        }
-
-         const deletedTask = await Todo.findByIdAndDelete(id);
-         console.log("deleteTask ~ deletedTask:", deletedTask);
-
-         if (!deletedTask) {
-           return next(errorHandler(404, "Task not found"));
-         }
-
-         res.status(200).json({
-           success: true,
-           message: "Task deleted successfully",
-         });
-         
-    } catch (error) {
-        next(error);
+    if (!id) {
+      return next(errorHandler(400, "Id Not Found"));
     }
+
+    const deletedTask = await Todo.findByIdAndDelete(id);
+    console.log("deleteTask ~ deletedTask:", deletedTask);
+
+    if (!deletedTask) {
+      return next(errorHandler(404, "Task not found"));
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Task deleted successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const completeTask = async (req, res, next) => {
+  try {
+    const { id } = req.body;
+
+    if (!id) {
+      return next(errorHandler(400, "Id Not Found"));
+    }
+
+    await Todo.findByIdAndUpdate(id, {
+      complete: true,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Task Status  Updated successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
 };
--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -1,11 +1,16 @@
 import express from "express";
-import { createTask, deleteTask, getAllTask } from "../controllers/todoContoller.js";
+import {
+  completeTask,
+  createTask,
+  deleteTask,
+  getAllTask,
+} from "../controllers/todoContoller.js";
 
 const router = express.Router();
 
 router.post("/create", createTask);
 router.get("/getall", getAllTask);
 router.delete("/delete", deleteTask);
-
+router.post("/complete", completeTask);
 
 export default router;
